73d6d09bc7af2e73353aa33bf0ff802bfb8b07ba
dhruv2185 <dhruvghevariya2002@gmail.com>
2024-02-07 00:35:11 +0530

    remove user profile from register response api

--- netropolis_backend/backend/login_views.py
+++ netropolis_backend/backend/login_views.py
@@ -22,14 +22,9 @@ class RegisterView(generics.ListCreateAPIView):
         if serializer.is_valid():
             user = serializer.save()
             refresh = RefreshToken.for_user(user)
-            print(refresh)
             return Response({
                 'refresh': str(refresh),
                 'access': str(refresh.access_token),
-                'username': user.username,
-                'email': user.email,
-                'first_name': user.first_name,
-                'last_name': user.last_name,
             }, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
