b02f402b8c26fd83cf3a9738a0403f834acea851
beakbreak <namanbhagat1414@gmail.com>
2024-02-10 20:25:08 +0530

    models commit (applications left)

--- netropolis_backend/backend/admin.py
+++ netropolis_backend/backend/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Teams, Community_Managers, Quests, Schedules
+from .models import Teams, Community_Managers, Quests, Schedules, Applications
 
 # Register your models here.
 admin.site.register(Teams)
 admin.site.register(Community_Managers)
 admin.site.register(Quests)
 admin.site.register(Schedules)
+admin.site.register(Applications)
